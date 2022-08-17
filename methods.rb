b7756c88744821836ddc1000c99c28d72fc0eafa
Marymuriga <marymuriga@gmail.com>
2022-08-17 09:17:53 +0300

    commit tree 11ecd90d09eb95cae74023bd44d0478932f602ff

--- methods.rb
+++ methods.rb
@@ -1,1 +1,45 @@
 # Your code here!
+
+
+def greet_programmer
+   puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet(name ="Naureen")
+    puts "Hello, #{name}!"
+end
+greet
+
+def greet(name ="Jimmy")
+   puts "Hello, #{name}!"
+end
+greet
+
+def greet_with_default(name = "programmer")
+   puts "Hello, #{name}!"
+end
+greet_with_default("Naureen")
+greet_with_default()
+
+
+ def add(num1, num2)
+      num1 +  num2
+     
+ end
+ sum =  add(2, 3)
+ puts sum
+ 
+
+  def halve(number) 
+    if number.class != Integer
+      return nil
+     else
+       return number / 2;
+     end
+  end
+  halve(4)
+  
+
+
+ 
